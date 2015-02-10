601685f83322fa5f2204c87acafe8b216bee780c
modshirnk <hello@modshrink.com>
2015-02-10 23:21:08 +0900

    gulp導入

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require("gulp");
+
+var plumber = require("gulp-plumber");
+var sass = require("gulp-sass");
+
+gulp.task("sass", function() {
+	gulp.src("sass/**/*.scss")
+		.pipe(plumber())
+		.pipe(sass())
+		.pipe(gulp.dest("./"));
+});
