1068758ab9aa35809e2737a654142d8142015115
mayoibi <mayoibi@modshrink.com>
2015-02-12 04:43:09 +0900

    gulp関連監視対象変更

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,8 @@
-var gulp = require("gulp");
-
-var plumber = require("gulp-plumber");
-var sass = require("gulp-sass");
-
-gulp.task("sass", function() {
-	gulp.src("sass/**/*.scss")
-		.pipe(plumber())
-		.pipe(sass())
-		.pipe(gulp.dest("./"));
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+ 
+gulp.task('sass', function () {
+    gulp.src('./sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('.'));
 });
