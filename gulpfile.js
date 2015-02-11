befa4928d75d2981874ba4069ccc863321a56719
mayoibi <mayoibi@modshrink.com>
2015-02-12 05:02:01 +0900

    watchタスク, gulp-plumber 追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+var plumber = require('gulp-plumber');
+
 gulp.task('sass', function () {
     gulp.src('./sass/*.scss')
+        .pipe(plumber())
         .pipe(sass())
         .pipe(gulp.dest('.'));
 });
+
+gulp.task('watch', function(){
+  gulp.watch('./sass/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass']);
