46d293b09b9873bf848836dc10706a7d14682e21
mayoibi <mayoibi@modshrink.com>
2015-02-16 04:24:38 +0900

    copyタスク追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
+var path = require('path');
 var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
+
+var paths = {
+
+}
 
 gulp.task('sass', function () {
     gulp.src('./sass/*.scss')
@@ -9,8 +14,14 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('.'));
 });
 
+gulp.task('copy', function() {
+    gulp.src('.')
+        .pipe(plumber())
+        .pipe(gulp.dest('/Users/mayoibi/vagrant-wp-dev/www/wordpress/wp-content/themes/modshrink_s'));
+});
+
 gulp.task('watch', function(){
-  gulp.watch('./sass/*.scss', ['sass']);
+  gulp.watch('./sass/*.scss', ['sass', 'copy']);
 });
 
 gulp.task('default', ['sass']);
