f28daf61317cac9862ad04e3b735f48ef7c06da3
mayoibi <mayoibi@modshrink.com>
2015-02-18 06:09:26 +0900

    削除タスク追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 var gulp = require('gulp');
 var path = require('path');
 var sass = require('gulp-sass');
+var del = require('del');
+var vinylPaths = require('vinyl-paths');
 var plumber = require('gulp-plumber');
-
-var paths = {
-
-}
 
 gulp.task('sass', function () {
     gulp.src('./sass/*.scss')
@@ -14,14 +12,29 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('.'));
 });
 
-gulp.task('copy', function() {
-    gulp.src('.')
-        .pipe(plumber())
-        .pipe(gulp.dest('/Users/mayoibi/vagrant-wp-dev/www/wordpress/wp-content/themes/modshrink_s'));
+gulp.task('clean', function(cb) {
+  del(['/Users/mayoibi/vagrant-wp-dev/www/wordpress/wp-content/themes/modshrink_s/*'], {force: true}, cb);
+});
+
+gulp.task('copy', ['clean'], function() {
+    gulp.src('images/')
+        .pipe(gulp.dest('/Users/mayoibi/vagrant-wp-dev/www/wordpress/wp-content/themes/modshrink_s/'));
+    gulp.src('inc/')
+        .pipe(gulp.dest('/Users/mayoibi/vagrant-wp-dev/www/wordpress/wp-content/themes/modshrink_s/'));
+    gulp.src('js/')
+        .pipe(gulp.dest('/Users/mayoibi/vagrant-wp-dev/www/wordpress/wp-content/themes/modshrink_s/'));
+    gulp.src('languages/')
+        .pipe(gulp.dest('/Users/mayoibi/vagrant-wp-dev/www/wordpress/wp-content/themes/modshrink_s/'));
+    gulp.src('*.php')
+        .pipe(gulp.dest('/Users/mayoibi/vagrant-wp-dev/www/wordpress/wp-content/themes/modshrink_s/'));
+    gulp.src('*.css')
+        .pipe(gulp.dest('/Users/mayoibi/vagrant-wp-dev/www/wordpress/wp-content/themes/modshrink_s/'));
+    gulp.src('screenshot.png')
+        .pipe(gulp.dest('/Users/mayoibi/vagrant-wp-dev/www/wordpress/wp-content/themes/modshrink_s/'));
 });
 
 gulp.task('watch', function(){
-  gulp.watch('./sass/*.scss', ['sass', 'copy']);
+  gulp.watch('./sass/*.scss', ['sass', 'clean', 'copy']);
 });
 
 gulp.task('default', ['sass']);
